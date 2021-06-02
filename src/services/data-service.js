6ab508a77ab50c42fe8a47aa8847da3375895aef
Jesus <Merlos>
2021-06-01 20:58:45 -0400

    pusblish setup done

--- src/services/data-service.js
+++ src/services/data-service.js
@@ -1,7 +1,7 @@
 
 export const getCustomerTransaction = () => {
     return new Promise((resolve, reject) => {
-        fetch('http://localhost:3000/data/customer-rewards.json',{
+        fetch('https://jmerlosrepo.github.io/customer-rewards/data/customer-rewards.json',{
         headers : { 
           'Content-Type': 'application/json',
           'Accept': 'application/json'
